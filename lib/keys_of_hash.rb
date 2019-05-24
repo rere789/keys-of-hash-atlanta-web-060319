418081625bc21ad21a70c1cf2ff39202025751dd
Reina Ewing <reina.castro3@gmail.com>
2019-05-24 15:07:02 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,6 @@ require "pry"
 class Hash
   def keys_of(arguments)
     # code goes here
-    binding.pry 
-  arguments.each do |key, value|
-    return value if key == "Panama"
-    end 
+    returns *arguments
 end
 end 
