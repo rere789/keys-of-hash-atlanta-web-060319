ed207d3c7d763b7ca5feab220a4eddef5fd74225
Reina Ewing <reina.castro3@gmail.com>
2019-05-24 15:21:27 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,9 @@
 require "pry"
 
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-    hash = *arguments
-    # return hash 
-    hash.each do |key, value|
+    arguments.each do |key, value|
       return hash.keys if hash == "Panama"
     end 
   end 
