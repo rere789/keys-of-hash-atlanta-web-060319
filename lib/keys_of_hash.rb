6429f52588b4ca1dcd59f6aa419f51618b3a684e
Reina Ewing <reina.castro3@gmail.com>
2019-05-24 15:30:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     # code goes here
-    animals = {}
+    animals = []
       self.each do |animal ,home|
         if arguments.include?(home)
           animals << animal
