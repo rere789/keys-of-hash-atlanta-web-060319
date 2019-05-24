7493c1a08b4ea584a87eb9e40863af1e35651a5c
Reina Ewing <reina.castro3@gmail.com>
2019-05-24 15:17:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
   def keys_of(arguments)
     # code goes here
     hash = *arguments
-    return hash 
+    # return hash 
       return hash.keys if hash.has_value?("Panama")
   end 
 end
